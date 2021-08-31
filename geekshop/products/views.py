e2aacdbc80921c0d38c5ecbfacf33646dc4fd517
tdm <dtanukov@gmail.com>
2021-08-31 12:34:49 +0500

    добавил дату и шаблонный тег, в заголовок страницы

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,5 +1,8 @@
+import time
+
 from django.shortcuts import render
 
+from datetime import date
 import os
 import json
 
@@ -8,12 +11,15 @@ MODULE_DIR = os.path.dirname(__file__)
 # Create your views here.
 # Контроллер функции
 def index(request):
-    context = {'title': 'GeekShop'}
+    context = {'title': 'Главная',
+               'curr_date': date.today()}
 
     return render(request, 'index.html', context)
 
 def products(request):
-    context = {'title': 'Каталог'}
+    context = {'title': 'Каталог',
+               'curr_date': date.today()}
+
     file_content = os.path.join(MODULE_DIR, 'fixtures/goods.json')
     context['products'] = json.load(open(file_content, encoding='utf-8'))
 
