ea9ec32cc5a2f70a53bc1ec2f4074a885f8cbb82
tdm <dtanukov@gmail.com>
2021-08-31 19:30:31 +0500

    диспетчер URL в приложении

--- geekshop/products/urls.py
+++ geekshop/products/urls.py
@@ -15,16 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
-from products.views import index,products
+app_name = 'products'
+
+from products.views import products
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('products.urls', namespace='products')),
+   path('', products, name='index'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
