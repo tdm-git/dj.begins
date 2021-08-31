ac80f65f5efcf65c8d0041a3bc102f87e67b87c5
tdm <dtanukov@gmail.com>
2021-08-31 12:04:42 +0500

    задание со *-кой

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
+import os
+import json
+
+MODULE_DIR = os.path.dirname(__file__)
+
 # Create your views here.
 # Контроллер функции
 def index(request):
-    return render(request, 'index.html')
+    context = {'title': 'GeekShop'}
+
+    return render(request, 'index.html', context)
 
 def products(request):
-    return render(request, 'products.html')
+    context = {'title': 'Каталог'}
+    file_content = os.path.join(MODULE_DIR, 'fixtures/goods.json')
+    context['products'] = json.load(open(file_content, encoding='utf-8'))
+
+    return render(request, 'products.html', context)
