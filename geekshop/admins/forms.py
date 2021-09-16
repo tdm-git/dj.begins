b4b330493658fa47e92ab42a129980ac91557fae
tdm <dtanukov@gmail.com>
2021-09-16 13:00:02 +0500

    CRUD методы для товара

--- geekshop/admins/forms.py
+++ geekshop/admins/forms.py
@@ -33,23 +33,20 @@ class ProductsAdminForm(forms.ModelForm):
 
     name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Наименование'}))
     description = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Описание'}))
-
     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
-
     price = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control py-4', 'placeholder': 'Цена'}))
     quantity = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control py-4', 'placeholder': 'Количество'}))
-    # category = forms.ChoiceField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Категория'}))
     # category = forms.ChoiceField(widget=forms.Select(
     #     attrs={'class': 'form-control', 'placeholder': 'Service used as base of this service pool'}),
     #     label='Base service',choices=ProductsCategory.objects.all()
     #     )
-    category = forms.ChoiceField(widget=forms.Select(attrs={'class':'form-control py-4', 'placeholder':'Категория'}))
+    category = forms.ChoiceField(widget=forms.Select(attrs={'class':'form-control py-4', 'placeholder':'Категория товара'}))
 
     class Meta:
         model = User
         fields = ('name', 'image', 'description', 'price', 'quantity', 'category')
 
-    def init(self, *args, **kwargs):
-        super(ProductsAdminForm, self).init(*args, **kwargs)
-        service = ProductsCategory.objects.values_list('id')
+    def __init__(self, *args, **kwargs):
+        super(ProductsAdminForm, self).__init__(*args, **kwargs)
+        service = ProductsCategory.objects.values_list('id', 'name')
         self.fields['category'].choices = service
